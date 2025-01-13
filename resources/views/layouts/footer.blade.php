80b3d10e85b1a0b7f32460878e142d9d4bd31022
Vipurthanan <lessandsass@gmail.com>
2025-01-13 16:50:33 +0530

    Creating Livewire components
    - Creating the TaskCreate components inside the tasks directory using the artisan command.
    - The routing, the component can be work standalone pages, that has to be registered as a separate route.
    - Test whether component was rendered by navigating through the URL.
    - So we can remove the components directory in the livewire config file.
    - The footer.blade.php file was edited with my personal links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,31 +2,32 @@
     <p class="font-bold text-gray-200">
         This template has been created by
         <a
-            href="https://www.youtube.com/c/codewithdary"
+            href="https://www.youtube.com"
             class="text-pink-400 transition-all hover:text-pink-500"
             target="_blank">
-            Code With Dary
+            Code With Vipu
         </a>
     </p>
 
     <ul class="py-4">
         <li class="inline pr-4 text-xl text-white transition-all hover:text-green-400">
-            <a href="https://www.patreon.com/user?u=30307830">
+            <a href="https://www.patreon.com">
                 <i class="fa-brands fa-patreon"></i>
             </a>
         </li>
         <li class="inline pr-4 text-xl text-white transition-all hover:text-green-400">
-            <a href="https://www.youtube.com/@codewithdary">
+            <a href="https://www.youtube.com/@metalkingwithanai-tm9zx">
+
                 <i class="fa-brands fa-youtube"></i>
             </a>
         </li>
         <li class="inline pr-4 text-xl text-white transition-all hover:text-green-400">
-            <a href="https://twitter.com/codewithdary">
+            <a href="https://x.com/lessandsass">
                 <i class="fa-brands fa-twitter"></i>
             </a>
         </li>
         <li class="inline pr-4 text-xl text-white transition-all hover:text-green-400">
-            <a href="https://www.instagram.com/codewithdary/">
+            <a href="https://www.instagram.com/lessandsass/">
                 <i class="fa-brands fa-instagram"></i>
             </a>
         </li>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Tasks\TaskCreate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks/create', TaskCreate::class);
+
